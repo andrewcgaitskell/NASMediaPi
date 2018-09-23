9de8586ea6de3d4e01573a51078b543cec0d9440
andrewcgaitskell <andrew@gaitskell.com>
2018-09-23 13:36:39 +0100

    Update FileList.py

--- FileList.py
+++ FileList.py
@@ -13,10 +13,10 @@ def get_information(directory):
     for i in os.listdir(directory):
         a = os.stat(os.path.join(directory,i))
         fullfilename = i
-        justfilename,justfileextension = fullfilename.split(".")
+        justfilenamesplit = fullfilename.split(".")
         lastmodifieddate = time.ctime(a.st_atime)
         createddate = time.ctime(a.st_ctime)
-        file_list.append([justfilename,justfileextension,lastmodifieddate,createddate]) #[file,most_recent_access,created]
+        file_list.append([justfilenamesplit,lastmodifieddate,createddate]) #[file,most_recent_access,created]
     return file_list
 
 a = get_information("/mnt/PIHDD/data")
