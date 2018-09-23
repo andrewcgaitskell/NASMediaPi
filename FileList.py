e01a5c7d952996d26d0be70a8279a882bef816c2
andrewcgaitskell <andrew@gaitskell.com>
2018-09-23 13:23:12 +0100

    Update FileList.py

--- FileList.py
+++ FileList.py
@@ -1,4 +1,4 @@
-import os
+import os,time
 
 def files(path):  
     for file in os.listdir(path):
@@ -7,3 +7,12 @@ def files(path):
 
 for file in files("."):  
     print (file)
+
+def get_information(directory):
+    file_list = []
+    for i in os.listdir(directory):
+        a = os.stat(os.path.join(directory,i))
+        file_list.append([i,time.ctime(a.st_atime),time.ctime(a.st_ctime)]) #[file,most_recent_access,created]
+    return file_list
+
+print get_information("/mnt/PIHDD/data")
