add1a9ac5bb2385ca04b0f0b5e469cc4363a47a2
andrewcgaitskell <andrew@gaitskell.com>
2018-09-23 14:11:25 +0100

    Update FileList.py

--- FileList.py
+++ FileList.py
@@ -12,14 +12,19 @@ def get_information(directory):
     file_list = []
     for i in os.listdir(directory):
         a = os.stat(os.path.join(directory,i))
-        b = os.path.join(directory,i)
-        print(b)
+        fullpathtofile = os.path.join(directory,i)
+        # print(b)
         fullfilename = i
         if fullfilename[0] != "." :
+            foldersaslist = fullpathtofile.split(".")
+            foldercount = length(foldersaslist)
+            containingfolder = foldersaslist[foldercount]
             justfilenamesplit = fullfilename.split(".")
+            fileextension = justfilenamesplit[1]
+            filename = justfilenamesplit[0]
             lastmodifieddate = time.ctime(a.st_atime)
             createddate = time.ctime(a.st_ctime)
-            file_list.append([justfilenamesplit,lastmodifieddate,createddate]) #[file,most_recent_access,created]
+            file_list.append([containingfolder,filename,fileextension,lastmodifieddate,createddate]) #[file,most_recent_access,created]
     return file_list
 
 a = get_information("/mnt/PIHDD/data")
